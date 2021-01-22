c78a31024b6f7ad6136f7262ba69fbc1c02a7a53
XORveRCOM <contact@xorver.com>
2021-01-22 22:40:14 +0900

    setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -1,5 +1,25 @@
+@echo off
+
 set TARGET=%1
 set BASE=%~dp0
+
+if "%TARGET%" == "" (
+	echo setup.cmd targetfolder\
+	exit /B 1
+)
+
+cd %BASE%
+git pull
+
+set EXEID=launcher
+set BUILDEXE=%BASE%launcher\bin\Release\%EXEID%.exe
+set TARGETEXE=%TARGET%%EXEID%.exe
+
 set MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 %MSBUILD% %BASE%ziphttpdTool.sln -p:Configuration=Release
-copy %BASE%launcher\bin\Release\launcher.exe %TARGET%
+
+if exist %TARGETEXE%.old del /F %TARGETEXE%.old
+if exist %TARGETEXE% ren %TARGETEXE% %EXEID%.old
+copy %BUILDEXE% %TARGETEXE%
+
+exit /B 0
